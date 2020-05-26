3daaf41d8a0cd8172c5efad5f684f37092cae044
Amthor <anja.amthor@siemens-healthineers.com>
2020-05-26 16:18:38 +0200

    added my python file

---
+++ src/spots_heikotopic.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 26 14:01:51 2020
+
+@author: z003vjxs
+"""
+
+# USAGE
+# python detect_bright_spots.py --image images/lights_01.png
+
+# import the necessary packages
+from imutils import contours
+from skimage import measure
+import numpy as np
+import argparse
+import imutils
+import cv2
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True,
+	help="path to the image file")
+args = vars(ap.parse_args())
+
+# load the image, convert it to grayscale, and blur it
+image = cv2.imread('C:/Users/z003vjxs/Download/IMG_1627.JPG')
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+blurred = cv2.GaussianBlur(gray, (11, 11), 0)
+
+# threshold the image to reveal light regions in the
+# blurred image
+thresh = cv2.threshold(blurred, 200, 255, cv2.THRESH_BINARY)[1]
+
+# perform a series of erosions and dilations to remove
+# any small blobs of noise from the thresholded image
+thresh = cv2.erode(thresh, None, iterations=2)
+thresh = cv2.dilate(thresh, None, iterations=4)
+
+# perform a connected component analysis on the thresholded
+# image, then initialize a mask to store only the "large"
+# components
+labels = measure.label(thresh, neighbors=8, background=0)
+mask = np.zeros(thresh.shape, dtype="uint8")
+
+# loop over the unique components
+for label in np.unique(labels):
+	# if this is the background label, ignore it
+	if label == 0:
+		continue
+
+	# otherwise, construct the label mask and count the
+	# number of pixels 
+	labelMask = np.zeros(thresh.shape, dtype="uint8")
+	labelMask[labels == label] = 255
+	numPixels = cv2.countNonZero(labelMask)
+
+	# if the number of pixels in the component is sufficiently
+	# large, then add it to our mask of "large blobs"
+	if numPixels > 300:
+		mask = cv2.add(mask, labelMask)
+
+plt.imshow(blurred)
+
+from skimage import color
+from skimage import io
+
+img = color.rgb2gray(io.imread('C:/Users/z003vjxs/Desktop/Spots.PNG'))
