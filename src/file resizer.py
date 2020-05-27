1ad3ca8fb8e907020637b561f9375bcc7209e808
Michael Wagner <michael.wm.wagner@fau.de>
2020-05-27 16:34:42 +0200

    moved files, deleted unecessary bloat

--- src/file resizer.py
+++ src/file resizer.py
@@ -2,30 +2,26 @@ import cv2
 import numpy as np
 import os
 
-def size_down(img_path):
+
+# resizes a file by "resize_factor"
+def size_down(img_path, resize_factor):
     img = cv2.imread(img_path)
     height = img.shape[0]
     width = img.shape[1]
 
-    resize_factor = 5
 
-    print("resize to",  int(width/resize_factor), int(height/resize_factor))
-    img2 = cv2.resize(img, (int(width/resize_factor), int(height/resize_factor)))
+    print("resize to", int(width / resize_factor), int(height / resize_factor))
+    img2 = cv2.resize(img, (int(width / resize_factor), int(height / resize_factor)))
     return img2
 
-
+#iterates over dir and calls resize on all files (original files are lost)
 def main():
     for root, dirs, files in os.walk("../img/all", topdown=False):
         for f in files:
             print(os.path.join(root, f))
-            #try:
-            img = size_down(os.path.join(root, f))
+            # try:
+            img = size_down(os.path.join(root, f), 5)
             cv2.imwrite(os.path.join(root, f), img)
-
-            #except:
-                #print("failed")
-    #extract("../img/tee.png")
-    #extract("../img/dummy.png")
 
 
 if __name__ == "__main__":
